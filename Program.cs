48266ac628c5e1a6d1b2adf00d1c4ea2a4d3a4f4
Khalil <khalilaliev0@gmail.com>
2025-12-08 09:28:38 +0100

    remade with switch case

--- Program.cs
+++ Program.cs
@@ -1,40 +1,30 @@
 ﻿Console.Write("Enter number from 1 to 7: ");
-int dayOfTheWeak = Convert.ToInt32(Console.ReadLine());
+int dayOfTheWeek = Convert.ToInt32(Console.ReadLine());
 
-if (dayOfTheWeak >= 1 && dayOfTheWeak <= 7)
+switch (dayOfTheWeek)
 {
-    if (dayOfTheWeak == 1)
-    {
-        Console.WriteLine($"Your day of the weak is Monday");
-    }
-    if (dayOfTheWeak == 2)
-    {
-        Console.WriteLine($"Your day of the weak is Thuesday");
-    }
-    if (dayOfTheWeak == 3)
-    {
-        Console.WriteLine($"Your day of the weak is Wednesday");
-    }
-    if (dayOfTheWeak == 4)
-    {
-        Console.WriteLine($"Your day of the weak is Thursday");
-    }
-    if (dayOfTheWeak == 5)
-    {
-        Console.WriteLine($"Your day of the weak is Friday");
-    }
-    if (dayOfTheWeak == 6)
-    {
-        Console.WriteLine($"Your day of the weak is Saturday");
-    }
-    if (dayOfTheWeak == 7)
-    {
-        Console.WriteLine($"Your day of the weak is Sunday");
-    }
-
-}
-else
-{
-    Console.WriteLine($"Your {dayOfTheWeak} number incorrect");
-
+    case 1:
+        Console.WriteLine($"Your day of the week is Monday");
+        break;
+    case 2:
+        Console.WriteLine($"Your day of the week is Thuesday");
+        break;
+    case 3:
+        Console.WriteLine($"Your day of the week is Wednesday");
+        break;
+    case 4:
+        Console.WriteLine($"Your day of the week is Thursday");
+        break;
+    case 5:
+        Console.WriteLine($"Your day of the week is Friday");
+        break;
+    case 6:
+        Console.WriteLine($"Your day of the week is Saturday");
+        break;
+    case 7:
+        Console.WriteLine($"Your day of the week is Sunday");
+        break;
+    default:
+        Console.WriteLine($"Your {dayOfTheWeek} number incorrect");
+        break;
 }
